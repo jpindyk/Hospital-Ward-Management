1d0f674b67d780789c0b170221842c4437bfc03d
jpindyk <jakub.pindyk@gmail.com>
2022-06-28 21:46:48 +0200

    SpringBoot Starter

---
+++ src/main/java/com/hospitalwardmanagement/HospitalWardManagementApplication.java
@@ -0,0 +1,13 @@
+package com.hospitalwardmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalWardManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalWardManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hospitalwardmanagement/HospitalWardManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospitalwardmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalWardManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
