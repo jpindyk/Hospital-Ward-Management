38f75df106ea863d8eadff3403694de5c0925af2
jpindyk <jakub.pindyk@gmail.com>
2022-07-09 23:02:33 +0200

    PatientService updated

--- src/main/java/com/hospitalwardmanagement/service/PatientService.java
+++ src/main/java/com/hospitalwardmanagement/service/PatientService.java
@@ -1,4 +1,18 @@
 package com.hospitalwardmanagement.service;
 
+import com.hospitalwardmanagement.model.patient.Patient;
+
+import java.util.List;
+
 public interface PatientService {
+    Patient addPatient (Patient patient);
+    Patient updatePatientById (Long id, Patient patient);
+    Patient getPatientById (Long id, Patient patient);
+    Patient getPatientByPesel (String pesel);
+    List<Patient> getAllPatients();
+    List<Patient> getPatientsByHospitalRoom(Long hospitalRoomId);
+    List<Patient> getPatientsByDoctor(Long doctorId);
+    void deletePatient(Long id);
+
+
 }
