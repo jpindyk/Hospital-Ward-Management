5642cd806749650e34eab0114fa6e0a316957f61
jpindyk <jakub.pindyk@gmail.com>
2022-07-23 15:32:33 +0200

    createdByUser and lastChangeByUser added

--- src/main/java/com/hospitalwardmanagement/model/ObjectAudit.java
+++ src/main/java/com/hospitalwardmanagement/model/ObjectAudit.java
@@ -1,13 +1,18 @@
 package com.hospitalwardmanagement.model;
 
-import javax.persistence.Column;
-import javax.persistence.Embeddable;
-import javax.persistence.PrePersist;
-import javax.persistence.PreUpdate;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.hospitalwardmanagement.model.user.User;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Embeddable
+@Setter
+@Getter
 public class ObjectAudit {
+
     @Column(nullable = false, updatable = false)
     private LocalDateTime createdOn;
     private LocalDateTime updatedOn;
@@ -21,4 +26,15 @@ public class ObjectAudit {
     void preUpdate() {
         updatedOn = LocalDateTime.now();
     }
+
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn
+    @JsonIgnore
+    private User createdByUser;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn
+    @JsonIgnore
+    private User lastChangeByUser;
 }
