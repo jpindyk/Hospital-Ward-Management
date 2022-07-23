d18d17870a9bed93945267725cfab6a929b1d9dd
jpindyk <jakub.pindyk@gmail.com>
2022-07-23 15:30:33 +0200

    mapRolesToAuthorities added

--- src/main/java/com/hospitalwardmanagement/security/CustomUserDetailsService.java
+++ src/main/java/com/hospitalwardmanagement/security/CustomUserDetailsService.java
@@ -1,14 +1,20 @@
 package com.hospitalwardmanagement.security;
 
 import com.hospitalwardmanagement.model.user.User;
+import com.hospitalwardmanagement.model.user.UserRole;
 import com.hospitalwardmanagement.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -20,6 +26,13 @@ public class CustomUserDetailsService implements UserDetailsService {
                 ()-> new UsernameNotFoundException("User not found for the email: " + email)
         );
 
-        return new org.springframework.security.core.userdetails.User(existingUser.getEmail(), existingUser.getPassword(), new ArrayList<>());
+        return new org.springframework.security.core.userdetails.User(existingUser.getEmail(),
+                                                            existingUser.getPassword(),
+                                                            mapRolesToAuthorities(existingUser.getRoles()));
+    }
+
+    private Collection< ? extends GrantedAuthority> mapRolesToAuthorities(Set<UserRole> roles) {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
     }
 }
