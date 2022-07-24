35c7ef0f786209801c542f8c9554d244e22d3eb3
jpindyk <jakub.pindyk@gmail.com>
2022-07-25 00:15:53 +0200

    UserRole modified to ENUM

--- src/main/java/com/hospitalwardmanagement/model/user/UserRole.java
+++ src/main/java/com/hospitalwardmanagement/model/user/UserRole.java
@@ -1,5 +1,6 @@
 package com.hospitalwardmanagement.model.user;
 
+import com.hospitalwardmanagement.exceptions.ResourceNotFoundException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -7,14 +8,31 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
-@Entity
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
-@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false)
-public class UserRole {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-    private String name;
+
+public enum UserRole {
+    ROLE_ADMIN(1),
+    ROLE_NURSE_WARD(2),
+    ROLE_NURSE(3),
+    ROLE_REGISTRAR(4),
+    ROLE_DEFAULT_USER(5);
+
+    UserRole(int nr) {
+        this.nr = nr;
+    }
+
+    private int nr;
+
+
+    public static UserRole ofNr(int nr) {
+        UserRole[] userRoles = values();
+        for (UserRole userRole : userRoles) {
+            if (userRole.nr == nr) {
+                return userRole;
+            }
+        }
+        throw new ResourceNotFoundException("Role", "id", nr);
+    }
+
+
+
 }
